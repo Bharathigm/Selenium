b2690be260fc2aca2dc80a8b1de26a4dc7a6a66b
Bharathigm <bharathigm18gmail.com>
2021-02-25 22:33:27 +0530

    Tested

--- src/AutomationTesting/FirfoxTagnameLoc.java
+++ src/AutomationTesting/FirfoxTagnameLoc.java
@@ -1,25 +1,22 @@
-//write a script to click on link by using tagname locator for chrome.
+//write a script  to click on link by using tagname locator for Firfox .
 package AutomationTesting;
 
 import java.nio.channels.InterruptedByTimeoutException;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class ChromeTagnameLoc 
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class FirfoxTagnameLoc 
 {
     public static void main(String[] args) throws InterruptedByTimeoutException, InterruptedException
 	{
-		//specify type of the browser
-		 	String key= "webdriver.chrome.driver";
-		//specify path of drivers .exe file
-		String value = ".\\drivers\\chromedriver_win32\\chromedriver.exe";
-		//specify the path of drivers to server
-		 	System.setProperty(key, value);
-		// Launch empty chrome browser
-		 	WebDriver driver = new ChromeDriver();
+			//specify type of the browser
+            String key= "webdriver.gecko.driver";
+            //specify path of drivers .exe file
+            String value = ".\\drivers\\geckodriver-v0.29.0-win64\\geckodriver.exe";
+            //specify the path of drivers to server
+                 System.setProperty(key, value);
+            // Launch empty Firefox browser
+                 FirefoxDriver driver= new FirefoxDriver();
         //Absolute path of html file
         driver.get("file:///C:/Users/Ganesh/eclipse-workspace/Seleniumtest/src/AutomationTesting/Webpages/loc3.html");
         // find the element in the html file with tagname a ,anchor tag
@@ -34,5 +31,3 @@ public class ChromeTagnameLoc
 		driver.close();
 	}
 }
-    
-
